277ac7ca7c6fed1ac22949bc0a9ee9c9b5c67b86
1yr <the.root0@gmail.com>
2021-01-04 23:24:38 +0700

    Create p2.js

---
+++ p2.js
@@ -0,0 +1,10 @@
+function getParentUrl() {
+    var isInIframe = (parent !== window),
+        parentUrl = null;
+
+    if (isInIframe) {
+        parentUrl = document.referrer;
+    }
+
+    return parentUrl;
+}
